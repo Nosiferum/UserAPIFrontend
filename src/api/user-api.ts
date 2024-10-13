62424b8787a3dc651afd6f92b0e7f51713c88e62
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-10-13 11:19:06 +0300

    feat: Added user register endpoint call

---
+++ src/api/user-api.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const userApi = axios.create({
+    baseURL:  'http://localhost:3001',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+const registerUser = async (userData: any) => {
+    try {
+        const response = await userApi.post('/users/register', userData);
+        return response.data;
+    } catch (error: any) {
+        if (error.response && error.response.data) {
+            throw new Error(error.response.data.message || error.response.data.error || 'Registration failed');
+        } else {
+            throw new Error('Something went wrong');
+        }
+    }
+}
+
+
+
+export default {registerUser};
