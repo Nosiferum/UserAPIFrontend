2c9aad5e25792d2323a602787fc2248439158622
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-10-13 19:06:19 +0300

    feat: Added basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import './App.css'
 import Login from "./components/Login.tsx";
 import {Route, Routes} from "react-router-dom";
 import WelcomePage from "./components/WelcomePage.tsx";
-import UserForm from "./components/UserForm.tsx";
+import Register from "./components/Register.tsx";
+import NavBar from "./components/NavBar.tsx";
 
 function App() {
 
   return (
     <>
+        <NavBar/>
         <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<UserForm/>}/>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register/>}/>
             <Route path="/welcome" element={<WelcomePage/>}/>
         </Routes>
     </>
