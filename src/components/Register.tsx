88f410fa3560abe0ad1d5bc63b4a4bc2e76adc96
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-10-13 19:05:50 +0300

    feat: Added error feedback to the form. Added redirect to the form and also changed the file name to Register

---
+++ src/components/Register.tsx
@@ -0,0 +1,56 @@
+import {useState, FormEvent, ChangeEvent} from "react";
+import userApi from "../api/user-api.ts"
+import {useNavigate} from "react-router-dom";
+import "./Register.css"
+
+function Register() {
+    const [formData, setFormData] = useState({username: "", password: ""});
+    const navigate = useNavigate();
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (evt: FormEvent<HTMLFormElement>) => {
+        evt.preventDefault();
+        await userApi.registerUser(formData)
+            .then((response) => {
+                localStorage.setItem("token", response.token)
+                navigate("/welcome")
+                setFormData({username: "", password: ""})
+            })
+            .catch(err => setError(err.message || 'Login failed. Please try again.'));
+    }
+
+    const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
+        setFormData(currentData => {
+            return {...currentData, [evt.target.name]: evt.target.value}
+        });
+    }
+
+    return (
+        <div className="register-form">
+            <h1>Register</h1>
+            {error && <div className="error-message">{error}</div>}
+            <form onSubmit={handleSubmit}>
+                <h1>Username is: {formData.username} and password is: {formData.password}</h1>
+                <label htmlFor="username">Username</label>
+                <input type="text"
+                       placeholder="username"
+                       name="username"
+                       id="username"
+                       onChange={handleChange}
+                       value={formData.username}
+                />
+                <label htmlFor="password">Password</label>
+                <input type="text"
+                       placeholder="password"
+                       name="password"
+                       id="password"
+                       onChange={handleChange}
+                       value={formData.password}
+                />
+                <button>Sign up</button>
+            </form>
+        </div>
+    )
+}
+
+export default Register;
