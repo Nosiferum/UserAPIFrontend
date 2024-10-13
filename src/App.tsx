c1e86c52480b2ab6816e09cfdb49a4a5656343ad
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-10-13 14:54:09 +0300

    feat: Added routes for login, register and welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
+import Login from "./components/Login.tsx";
+import {Route, Routes} from "react-router-dom";
+import WelcomePage from "./components/WelcomePage.tsx";
 import UserForm from "./components/UserForm.tsx";
 
 function App() {
 
   return (
     <>
-      <UserForm/>
-
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<UserForm/>}/>
+            <Route path="/welcome" element={<WelcomePage/>}/>
+        </Routes>
     </>
   )
 }
